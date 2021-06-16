4150efaa1d47e469cf8e40e534efe9354734c4f3
Pete Gadomski <pete.gadomski@gmail.com>
2021-06-16 13:57:43 -0600

    Don't check cog, but check size

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,9 +1,10 @@
 import os.path
 from tempfile import TemporaryDirectory
 
+import rasterio
+
 from stactools.gap.commands import create_gap_command
 from stactools.testing import CliTestCase
-from stactools.testing.validate_cloud_optimized_geotiff import validate
 from tests import test_data
 
 
@@ -23,7 +24,8 @@ class CommandsTest(CliTestCase):
                              msg="\n{}".format(result.output))
             for path in (os.path.join(directory, file)
                          for file in os.listdir(directory)):
-                warnings, errors, details = validate(path, full_check=True)
-                # why doesn't python have a `.is_empty()` method?
-                self.assertFalse(warnings, warnings)
-                self.assertFalse(errors, errors)
+                with rasterio.open(path) as dataset:
+                    data = dataset.read(1)
+                    rows, cols = data.shape
+                    self.assertLessEqual(rows, 500001)
+                    self.assertLessEqual(cols, 500001)
