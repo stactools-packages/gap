36f92d6ebba27eae96fbd14413082cb06c366dde
Pete Gadomski <pete.gadomski@gmail.com>
2021-06-17 10:46:10 -0600

    Allow tiffs to modify the bounds

--- src/stactools/gap/stac.py
+++ src/stactools/gap/stac.py
@@ -1,20 +1,37 @@
 import os.path
+from typing import Optional
 
 from pystac import Item
+import rasterio
+from shapely.geometry import mapping, box, shape
+
+from stactools.core.projection import reproject_geom
 
 from stactools.gap import Metadata
 
 
-def create_item(href: str) -> Item:
+def create_item(xml_href: str, tif_href: Optional[str] = None) -> Item:
     """Creates a STAC Item from an href to the metadata.
 
     Optionally allows a tile .tif to be provided, in which case the tile will be
     used to set the bounds instead of the XML metadata.
     """
-    metadata = Metadata.from_href(href)
-    item = Item(id=os.path.splitext(os.path.basename(href))[0],
-                geometry=metadata.geometry,
-                bbox=metadata.bbox,
+    metadata = Metadata.from_href(xml_href)
+    if tif_href:
+        id = os.path.splitext(os.path.basename(tif_href))[0]
+        with rasterio.open(tif_href) as dataset:
+            source_crs = dataset.crs
+            source_bbox = dataset.bounds
+            source_geometry = mapping(box(*source_bbox))
+        geometry = reproject_geom(source_crs, "EPSG:4326", source_geometry)
+        bbox = list(shape(geometry).bounds)
+    else:
+        id = os.path.splitext(os.path.basename(xml_href))[0]
+        geometry = metadata.geometry
+        bbox = metadata.bbox
+    item = Item(id=id,
+                geometry=geometry,
+                bbox=bbox,
                 properties={},
                 datetime=metadata.datetime)
     return item
--- tests/test_stac.py
+++ tests/test_stac.py
@@ -23,4 +23,18 @@ class StacTest(unittest.TestCase):
         # TODO check proj extension
         item.validate()
 
-    # TODO check tif suppliement
+    def test_create_item_with_tif(self):
+        infile = test_data.get_path(
+            "data-files/GAP_LANDFIRE_National_Terrestrial_Ecosystems_2011.XML")
+        tif = test_data.get_path(
+            "data-files/gap_landfire_nationalterrestrialecosystems2011_subset.tif"
+        )
+        item = create_item(infile, tif)
+        self.assertEqual(
+            item.id, "gap_landfire_nationalterrestrialecosystems2011_subset")
+        self.assertIsNotNone(item.geometry)
+        self.assertEqual(item.bbox, [
+            -110.00025443243528, 29.359168172694346, -99.94710283198935,
+            40.69658382856684
+        ])
+        item.validate()
